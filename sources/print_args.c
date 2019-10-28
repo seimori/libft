b866a1d91e0e3507da914aeeb47800c72d6bee5b
Issei MORIMOTO <imorimot@student.42.fr>
2019-10-28 13:19:42 +0100

    Fix print_percent width wrong return value

--- sources/print_args.c
+++ sources/print_args.c
@@ -6,7 +6,7 @@
 /*   By: imorimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 14:12:49 by imorimot          #+#    #+#             */
-/*   Updated: 2019/10/27 19:41:57 by imorimot         ###   ########.fr       */
+/*   Updated: 2019/10/27 20:04:57 by imorimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int				print_arg(va_list args, t_specs *specs, fp_arg *print)
 {
 	t_arg		arg;
-	int			arg_len;
+	int			count;
 
 	if (specs->conversion == 'f')
 		if (specs->lengthmodifier & BIG_L)
@@ -24,10 +24,10 @@ int				print_arg(va_list args, t_specs *specs, fp_arg *print)
 			arg = (t_arg)va_arg(args, double);
 	else
 		arg = (t_arg)va_arg(args, t_arg);
-	arg_len = print[specs->typeindex](arg, specs);
+	count = print[specs->typeindex](arg, specs);
 	if (specs->flags & MINUS && specs->conversion == 's')
-		arg_len = print_width_string(specs) + specs->arg_len;
+		count = print_width_string(specs) + specs->arg_len;
 	else if (specs->flags & MINUS)
-		arg_len = print_width(specs) + specs->arg_len;
-	return (arg_len);
+		count = print_width(specs) + specs->arg_len;
+	return (count);
 }
--- sources/print_percent.c
+++ sources/print_percent.c
@@ -6,7 +6,7 @@
 /*   By: imorimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 18:22:42 by imorimot          #+#    #+#             */
-/*   Updated: 2019/10/27 19:23:28 by imorimot         ###   ########.fr       */
+/*   Updated: 2019/10/28 13:18:51 by imorimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int			print_percent(t_arg arg, t_specs *specs)
 	(void)arg;
 	specs->arg_len = 1;
 	if (!(specs->flags & MINUS))
-		specs->arg_len = print_options(specs->arg_len, specs);
+		specs->spaces_len = print_width(specs);
 	ft_putchar('%');
-	return (specs->arg_len);
+	return (specs->arg_len + specs->spaces_len);
 }
